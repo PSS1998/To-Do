d4eabed521f683fc49f6d19164693f0d8f0bab96
PSS <PSS1998>
2020-12-17 23:23:30 +0330

    Add PUT and DELETE to genre API

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,16 +1,30 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Param, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import GenreService from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
+import UpdateGenreDto from './dto/update-genre.dto';
 
 @Controller('genre')
 export default class GenreController {
   constructor(private readonly genreServices: GenreService) {}
+
   @Post('post')
   postGenre( @Body() genre: CreateGenreDto) {
     return this.genreServices.insert(genre);
   }
+
   @Get()
   getAll() {
     return this.genreServices.getAllGenre();
   }
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: string) {
+    return this.genreServices.deleteGenre(+id);
+  }
+  
+  @Put('update/:id')
+  update(@Param('id') id: string, @Body() updateGenreDto: UpdateGenreDto) {
+    return this.genreServices.updateGenre(+id, updateGenreDto);
+  }
+
 }
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import CreateGenreDto from './dto/create-genre.dto';
+import UpdateGenreDto from './dto/update-genre.dto';
 import GenreEntity from '../db/genre.entity';
 
 @Injectable()
 export default class GenreService {
-    async insert(genreDetails: CreateGenreDto): Promise<GenreEntity> {
+  async insert(genreDetails: CreateGenreDto): Promise<GenreEntity> {
 
     const genreEntity: GenreEntity = GenreEntity.create();
     const {type} = genreDetails;
@@ -13,7 +14,21 @@ export default class GenreService {
     await GenreEntity.save(genreEntity);
     return genreEntity;
   }
+
   async getAllGenre(): Promise<GenreEntity[]> {
-        return await GenreEntity.find();
+    return await GenreEntity.find();
   }
+
+  async deleteGenre(genreID: number): Promise<any> {
+    return await GenreEntity.delete(genreID);
+  }
+
+  async updateGenre(genreID: number, genreDetails: UpdateGenreDto): Promise<GenreEntity> {
+    const genreEntity: GenreEntity = await GenreEntity.findOne({where: {id: genreID}});
+    const {type } = genreDetails;
+    genreEntity.type = type;
+    await GenreEntity.update(genreID, genreEntity);
+    return genreEntity;
+  }
+
 }
