b06443dc6d39c313f6877ed6e1707784887c50fd
PSS <PSS1998>
2020-12-18 18:45:21 +0330

    Clean Up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HelloModule } from './hello/hello.module';
-import BooksModule from './books/books.module';
-import GenreModule from './genre/genre.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TodoModule } from './todo/todo.module';
-import BookEntity from './db/book.entity';
-import GenreEntity from './db/genre.entity';
 import UserEntity from './db/user.entity';
 
 @Module({
-  imports: [HelloModule, BooksModule, GenreModule, UserModule,
+  imports: [UserModule,
     TypeOrmModule.forFeature(
-      [UserEntity, BookEntity , GenreEntity],
+      [UserEntity],
     ),
     TypeOrmModule.forRoot(),
     AuthModule,
--- src/books/dto/update-book.dto.ts
+++
@@ -1,5 +0,0 @@
-export default class UpdateBookDto {
-    readonly name: string;
-    readonly userID: number;
-    readonly genreIDs: number[];
-}
